620346576534a5e37f09cc34fc7c88674e7ad166
Sergii Severynov <ssever2010@gmail.com>
2019-08-07 14:44:55 +0300

    link component refactored to hooks

--- src/app/links/Link.tsx
+++ src/app/links/Link.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import moment from 'moment';
 import gql from 'graphql-tag';
-import { Mutation } from 'react-apollo';
 import _noop from 'lodash/noop';
+import { useMutation } from '@apollo/react-hooks';
 
 import { Link as LinkType } from '../../types';
 import { isLoggedIn } from '../../helpers';
@@ -33,37 +33,39 @@ type OwnProps = {
 };
 
 const Link: React.FC<OwnProps> =
-  ({link: { url, description, createdAt, postedBy, votes, id }, index, onUpdate = _noop}) => {
+  ({link: {url, description, createdAt, postedBy, votes, id}, index, onUpdate = _noop}) => {
+    const handleUpdate = (cache: any, {data: {vote}}: any) => {
+      onUpdate(cache, vote, id);
+    };
 
-  const handleUpdate = (cache: any, {data: {vote}}: any) => {
-    onUpdate(cache, vote, id);
+    const [voteMutation] = useMutation(VOTE_MUTATION, {
+      update: handleUpdate,
+      variables: {linkId: id}
+    });
+
+    const handleMutation = () => voteMutation();
+
+    return (
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{index + 1}.</span>
+          {isLoggedIn() && (
+            <div className="ml1 gray f11 vote" onClick={handleMutation}>
+              ▲
+            </div>
+          )}
+        </div>
+        <div className="ml1">
+          <div>
+            {description} ({url})
+          </div>
+          <div className="f6 lh-copy gray">
+            {votes && votes.length} votes | by{' '}
+            {postedBy ? postedBy.name : 'Unknown'}{' '} {moment(createdAt).fromNow()}
+          </div>
+        </div>
+      </div>
+    );
   };
-
-  return (
-    <div className="flex mt2 items-start">
-      <div className="flex items-center">
-        <span className="gray">{index + 1}.</span>
-        {isLoggedIn() && (
-          <Mutation mutation={VOTE_MUTATION} variables={{linkId: id}} update={handleUpdate}>
-            {(voteMutation: () => void) => (
-              <div className="ml1 gray f11 vote" onClick={voteMutation}>
-                ▲
-              </div>
-            )}
-          </Mutation>
-        )}
-      </div>
-      <div className="ml1">
-        <div>
-          {description} ({url})
-        </div>
-        <div className="f6 lh-copy gray">
-          {votes && votes.length} votes | by{' '}
-          {postedBy ? postedBy.name : 'Unknown'}{' '} {moment(createdAt).fromNow()}
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default Link;
